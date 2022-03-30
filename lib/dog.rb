d354d34b7ab772de63b5c5662479fc7a84058660
Zachary Sain <zachsain@gmail.com>
2022-03-30 13:13:29 -0500

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
+
+# Define a name getter and a name= setter method for the instance variable @name.
+# Define a breed getter and a breed= setter method for the instance variable @breed.
+
+
 class Dog
 
+    def name=(value)
+        @name = value
+    end 
+    def name
+        @name
+    end 
+
+    def breed=(value)
+        @breed = value
+    end 
+    def breed 
+        @breed
+    end 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-
+    def name=(value)
+        @name = value
+    end 
+    def name
+        @name
+    end
+    def job=(value)
+        @job = value
+    end 
+    def job
+        @job
+    end 
 end
